9f8cb23d907fb90f3c192f89871654eb51dd68e2
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-03 09:29:07 +0700

    reverse_array

--- Code_Gym/test.java
+++ Code_Gym/test.java
@@ -1,10 +1,26 @@
+import java.sql.Array;
 import java.util.*;
 
 public class test {
+    static void  sum(){
+        System.out.println("Tong");
+    }
     public static void main(String[] args) {
 
-        int [] a = {2,5,2,1,0};
-        Arrays.sort(a);
-        System.out.println(Arrays.toString(a));
+//        Map<String, Integer> map = new TreeMap<>();
+//        map.put("apple", 1);
+//        map.put("banana", 2);
+//        map.put("orange", 3);
+//
+//        for(Map.Entry<String,Integer> x: map.entrySet())
+//        {
+//
+//            x.setValue(x.getValue() + 1);
+//        }
+//
+//
+//        System.out.println(Arrays.toString(map.values().toArray()));
+
+    sum();
     }
 }
