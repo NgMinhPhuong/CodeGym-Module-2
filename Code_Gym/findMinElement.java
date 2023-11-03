eb4ba82cc1307576515a4765f267420baa09c38c
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-03 10:59:37 +0700

    update

--- Code_Gym/findMinElement.java
+++ Code_Gym/findMinElement.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class findMinElement {
     public static void main(String[] args) {
-        int a [] = {1,2,3,4,5};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size = sc.nextInt();
+        int a [] = new int[size];
+        for(int i = 0; i < size; i++)
+        {
+            System.out.print(i + "th Element: ");
+            a[i] = sc.nextInt();
+        }
         int min = a[0];
         for(int x : a)
             if(x < min) min = x;
