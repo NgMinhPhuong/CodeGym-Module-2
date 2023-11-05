3bbec0ccd1467a53a1a167c6ac9c6a3847238f17
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-05 09:58:03 +0700

    Triển khai interface

---
+++ Code_Gym/test/test1.java
@@ -0,0 +1,8 @@
+package test;
+
+public class test1 {
+    public int a = 2;
+    test1(int c){
+        this.a = c;
+    }
+}
---
+++ Code_Gym/test/tets.java
@@ -0,0 +1,14 @@
+package test;
+
+public class tets implements Comparable<test1>{
+
+
+    public int compareTo(test1 a, test1 b) {
+        return 1;
+    }
+
+    @Override
+    public int compareTo(test1 o) {
+        return 0;
+    }
+}
