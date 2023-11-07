9d4fe44469114ec7ec83d26843425117cf87c7f7
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-07 17:26:51 +0700

    Test Map

---
+++ test/src/cha.java
@@ -0,0 +1,5 @@
+public class cha {
+    void methodCha(){
+        System.out.println("cha");
+    }
+}
---
+++ test/src/child.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class child extends  cha{
+    void methodCon(){
+        System.out.println("con");
+    }
+
+    public static void main(String[] args) {
+       Bina
+
+    }
+}
