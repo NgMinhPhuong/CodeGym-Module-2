6e7131061cb31e285ea27fdb778cd17c4e8a91b1
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-07 22:15:51 +0700

    Luyện tập Collections Fw

--- test/src/cha.java
+++ test/src/cha.java
@@ -1,5 +1,22 @@
-public class cha {
-    void methodCha(){
-        System.out.println("cha");
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class cha  {
+    public static void main(String[] args) {
+        Map<String, Integer> mp = new HashMap<>();
+        mp.put("ph", 1);
+        mp.put("pa", 2);
+        mp.put("pb", 3);
+        mp.put("pp", 5);
+
+
+        Set<String> s = new HashSet<>();
+        s = mp.keySet();
+        s.remove("pp");
+        for(Map.Entry<String, Integer> x : mp.entrySet()){
+            System.out.println(x.getKey() + " " + x.getValue());
+        }
     }
 }
