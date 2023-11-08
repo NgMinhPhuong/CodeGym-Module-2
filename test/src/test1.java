51b6780e0f35e98a6e4e5dc5d8cad16c14cde242
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-08 14:26:25 +0700

    Binary Search

---
+++ test/src/test1.java
@@ -0,0 +1,26 @@
+import java.util.Comparator;
+
+public class test1<E extends Comparator<E>>  {
+    E data;
+    test1(){
+
+    }
+
+
+
+    void ham(){
+        
+    }
+    public static void main(String[] args) {
+        test1<Integer> a = new test1<>();
+
+    }
+
+
+
+
+
+
+
+
+}
