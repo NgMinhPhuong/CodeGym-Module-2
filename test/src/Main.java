4051a547469e9da95e3f9eb6b6b2a308ef221d0e
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-12 12:27:48 +0700

    Validate số điện thoại

--- test/src/Main.java
+++ test/src/Main.java
@@ -7,8 +7,8 @@ class Main{
 
         String s = "nguyenminhphuongaaaaa831@gmail.com";
         String s1 = "22/2/2000";
-        String s2 = "abcde";
-        String pattern = "abcd";
+        String s2 = "(";
+        String pattern = "\\(";
         Pattern pattern1 = Pattern.compile(pattern);
         Matcher matcher = pattern1.matcher(s2);
         System.out.println(s.matches(pattern));
