2f01f0ceeab50355df39375d04ca90f5f6694102
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-12 21:18:06 +0700

    Crawl tin tức

--- crawl-informatione-newspaper/src/NewspaperExample.java
+++ crawl-informatione-newspaper/src/NewspaperExample.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CrawlSongExample {
+public class NewspaperExample {
 
     public static void main(String[] args) {
         try {
--- test/src/Main.java
+++ test/src/Main.java
@@ -1,18 +1,13 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class Main{
-
+public class Main {
     public static void main(String[] args) {
+        String input = "\";
+        Pattern pattern = Pattern.compile("");
+        Matcher matcher = pattern.matcher(input);
+        System.out.println(matcher.matches());
 
-        String s = "nguyenminhphuongaaaaa831@gmail.com";
-        String s1 = "22/2/2000";
-        String s2 = "(";
-        String pattern = "\\(";
-        Pattern pattern1 = Pattern.compile(pattern);
-        Matcher matcher = pattern1.matcher(s2);
-        System.out.println(s.matches(pattern));
-        System.out.println(s1.matches(pattern));
-        System.out.println(s2.matches(pattern));
+
     }
 }
