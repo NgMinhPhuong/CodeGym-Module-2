463504a1e0b7a6c8847ea72afe3bf426e8dcb0f1
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-12 11:34:45 +0700

    Validate account

--- test/src/Main.java
+++ test/src/Main.java
@@ -5,16 +5,14 @@ class Main{
 
     public static void main(String[] args) {
 
-        String s = "1-2-2000";
+        String s = "nguyenminhphuongaaaaa831@gmail.com";
         String s1 = "22/2/2000";
-        String s2 = "abcdef";
-        String pattern = "\\d{1,2}[/-]\\d{1,2}[/-]\\d{4}";
+        String s2 = "abcde";
+        String pattern = "abcd";
         Pattern pattern1 = Pattern.compile(pattern);
         Matcher matcher = pattern1.matcher(s2);
-        System.out.println(matcher.matches());
         System.out.println(s.matches(pattern));
         System.out.println(s1.matches(pattern));
         System.out.println(s2.matches(pattern));
-        System.out.println(s2.replace("a", "aaa"));
     }
 }
