708773b8050edfef20dbbb2ad9ecfcb0ced0216c
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2023-11-15 14:07:09 +0700

    Triển khai Proxy Pattern

--- test/src/main.java
+++ test/src/main.java
@@ -1,7 +1,8 @@
 class main{
     public static void main(String[] args) {
         testcon a = new testcon();
-        testcha b= new testcha();
-        System.out.println(testcon.cnt);
+        testcha b = (testcha) a;
+        if(b instanceof testcha) System.out.println("yes");
+
     }
 }
